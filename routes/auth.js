53dd624935ba135be46a7f3bb5e462fa3e82c69a
sross9845 <sean.ross.985@gmail.com>
2019-11-05 12:00:00 -0800

    auth and login and signup routes

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/signup', (req,res) => {
+    res.send('hit the signup route')
+})
+router.post('/login', (req,res) => {
+    res.send('hit the login route')
+})
+router.post('/me/from/token', (req,res) => {
+    res.send('hit the verification route')
+})
+
+module.exports = router
